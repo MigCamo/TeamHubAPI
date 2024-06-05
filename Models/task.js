47bcb1386e6c8b0b147b190e143db99c6375199c
Miguel Camo <camomiguel1@gmail.com>
2024-06-05 17:17:03 -0600

    actualizacion

---
+++ Models/task.js
@@ -0,0 +1,45 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('task', {
+    IdTask: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    Name: {
+      type: DataTypes.STRING(50),
+      allowNull: true
+    },
+    Description: {
+      type: DataTypes.STRING(250),
+      allowNull: true
+    },
+    StartDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: true
+    },
+    EndDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: true
+    },
+    Status: {
+      type: DataTypes.STRING(90),
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'task',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "IdTask" },
+        ]
+      },
+    ]
+  });
+};
